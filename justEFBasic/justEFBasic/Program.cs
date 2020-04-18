7b59e8533a588d6a35eec7cda244ae575b3768d4
Bhargava Raju <Bhargava.Raju@csgi.com>
2020-04-18 22:24:01 +0530

    Added Microsoft.EntityFrameworkCore.SqlServer package to the project and also by using dbcontext scafolding dotnot core tool generate all the entities class files for all the domain object.

--- justEFBasic/justEFBasic/Program.cs
+++ justEFBasic/justEFBasic/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using justEFBasic.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace justEFBasic
 {
@@ -6,7 +10,38 @@ namespace justEFBasic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World of Database Programming");
+            try
+            {
+                using (AdventureWorks2017Context context = new AdventureWorks2017Context())
+                {
+                    //List<Address> address = new List<Address> { new Address { /*AddressId = 32526,*/  AddressLine1 = @"Sreecase Grande Apartment", City = @"Bangalore", PostalCode = "560103", AddressLine2 = "Kaadubeesanahalli", StateProvinceId = 3 }, new Address { /*AddressId = 32526,*/  AddressLine1 = @"BSR Paradise Apartment", City = @"Bangalore", PostalCode = "560103", AddressLine2 = "Boganhalli", StateProvinceId = 3 } };
+
+                    var address = context.Address.Find(32539);
+                    Console.WriteLine($"ADRESSS REMOVE  :  { address?.AddressLine1 }");
+                    context.Remove(address);
+                    context.SaveChanges();
+
+                    address = context.Address.Find(32538);
+                    Console.WriteLine($"ADRESSS REMOVE  :  { address?.AddressLine1 }");
+                    context.Remove(address);
+                    context.SaveChanges();
+
+                    //address.ForEach(add => context.Address.Add(add));
+                    //context.SaveChanges();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"This is SQL exception that occured : {ex.Message}");
+                Console.WriteLine($"Inner Exeption: {ex.InnerException}");
+                //Console.WriteLine($"Stack : {ex.StackTrace}");
+                Console.ReadKey();
+            }
+
+            
+
         }
     }
 }
